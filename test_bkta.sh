b39edf4d930173fbb8d7c386d2eed0ae2786b77f
Adrian Moldovan <adim.moldovan@gmail.com>
2023-07-03 15:08:39 +0300

    Run with collector 0.1.2

--- test_bkta.sh
+++
@@ -1,9 +0,0 @@
-ts=$(date +%s)
-export BUILDKITE_BRANCH="main"
-export BUILDKITE_BUILD_ID="id-$ts"
-export BUILDKITE_BUILD_URL="https://github.com"
-export BUILDKITE_COMMIT="aaa-bbb-ccc"
-export BUILDKITE_BUILD_NUMBER="1"
-export BUILDKITE_MESSAGE="local test run $ts"
-
-pytest
